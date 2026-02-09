1e6d7eaedfb2dad5bf293f84bf5b24f5f560739f
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-02-09 14:09:51 +0330

    set form

--- src/Component/shared/Form.jsx
+++ src/Component/shared/Form.jsx
@@ -8,38 +8,18 @@ export default function Form() {
           <div className="flext justify-center mt-auto ml-9">
             <CircleUser size={172} color="#fafafa" strokeWidth={0.5} />
           </div>
+          <form action="">
+
+          </form>
           <div className=" text-white  mb-1 ">
-            <h1 className="text-2xl ml-2"> Name</h1>
+            <label className="text-2xl ml-2"> Name</label>
             <input
               className="mt-1 p-1.5 pl-5 rounded-2xl w-60 border border-white"
               type="text"
               placeholder="Name"
             />
           </div>
-          <div className=" text-white mt-3 mb-2">
-            <h1 className="text-2xl ml-2">Last Name</h1>
-            <input
-              className="mt-1 p-1.5  pl-5 rounded-2xl w-60 border border-white"
-              type="text"
-              placeholder=" last Name"
-            />
-          </div>
-          <div className=" text-white mt-4 mb-2">
-            <h1 className="text-2xl ml-2"> Email</h1>
-            <input
-              className="mt-1 p-1.5  pl-5 rounded-2xl w-60 border border-white"
-              type="text"
-              placeholder="Email"
-            />
-          </div>
-          <div className=" text-white mt-4 mb-2">
-            <h1 className="text-2xl ml-2"> Password</h1>
-            <input
-              className="mt-1 p-1.5  pl-5 rounded-2xl w-60 border border-white"
-              type="number"
-              placeholder="Password"
-            />
-          </div>
+          
         </div>
       </>
     );
