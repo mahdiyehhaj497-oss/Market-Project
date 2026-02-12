38f1fdb3bdf7e6e386b91d442c18f7fe95968cec
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-02-12 12:11:43 +0330

    make button in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,19 +8,33 @@ export default function Home() {
     return (
       <>
         {/* <div className="bg-[url('src/assets/backgrond Gray.jpg')] rounded-3xl bg-no-repeat bg-cover bg-center w-screen h-screen mx-12px mb-40px absolute "> */}
-        <div className="bg-cream-100 w-screen h-screen">
+        <div className="bg-cream-100 w-screen h-screen ">
           <div className=" flex items-center  justify-center">
             <Navbar />
             {/* <h1 className=" text-white text-9xl font-extrabold font-serif pt-10 mt-5 ">
               welcom !
             </h1> */}
           </div>
-          <div className=" flex justify-center ">
-            <h1 className="mt-8 text-4xl font-bold font-serif">LET`S CREATE<br/> YOUR OWN STYLE</h1>
-            <Link to="/product">
-            hi
-            </Link>
-            <img src="src/assets/MacBook Air - 1 (2).jpg" alt="" className=" mt-7 "/>
+          <div className=" flex justify-center  ">
+            <div>
+              <h1 className="mt-8 text-4xl font-bold font-serif">
+                LET`S CREATE
+                <br /> YOUR OWN STYLE
+              </h1>
+              <div className="flex justify-center items-center mt-8">
+                <Link to="/product">
+                  <button className="p-3 rounded-2xl bg-lightg-100 font-bold text-redd-100">show products</button>
+                </Link>
+              </div>
+            </div>
+
+            
+              <img
+                src="src/assets/MacBook Air - 1 (2).jpg"
+                alt=""
+                className=" mt-7 "
+              />
+            
           </div>
         </div>
       </>
--- src/pages/Sing In.jsx
+++ src/pages/Sing In.jsx
@@ -3,7 +3,7 @@ import Form from "../Component/shared/Form";
 export default function SingIn() {
   return (
     <>
-      <div className="h-screen bg-gradient-to-r from-[#1B1F3A]  to-[#E1BF78] flex items-center justify-center mt-auto">
+      <div className="h-screen  from-[#1B1F3A]  to-[#E1BF78] flex items-center justify-center mt-auto">
         <div className="mb-auto mt-4">
           <Form />
           <h1 className="text-white font-light ml-10 text-lg mt-10">Forget Password?</h1>
